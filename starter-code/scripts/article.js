f917c3e7a65329ce00a1011640a801e4352f1f0c
Julien Wilson <julienawilson@gmail.com>
2016-10-25 16:24:57 -0700

    added script tags to html and finished bject constructor

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -7,22 +7,18 @@ function Article (options) {
    a placeholder for the object that will ultimately be
    passed in. Use all of the properties in blogArticles
    to populate the new Article data that we'll use.  */
-   this.title = options.title;
+  this.title = options.title;
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
 };
 
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
-  1. author name
-  2. author url
-  3. article title
-  4. article body
-  5. publication*/
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-/* TODO: This cloned article is no longer a template, as it now
-has real data attached to it. Remove the class from this new article! */
+
   return $newArticle;
 };
 
@@ -38,8 +34,4 @@ blogArticles.sort(function(currentObject, nextObject) {
 
 blogArticles.forEach(function(ele) {
   articles.push(new Article(ele));
-});
-
-articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
 });
