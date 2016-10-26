3827e4cd4ced55a4c4846592cb73f266035651ec
Copenbacon <sclary50@gmail.com>
2016-10-25 17:32:58 -0700

    solved infinite loop on return jqueryNewArticle. Changed a .body to .html() so the html rendered correctly.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -29,12 +29,13 @@ Article.prototype.toHtml = function() {
   $newArticle.find('address a').text(this.author);
   $newArticle.find('address a').attr('href', this.authorUrl);
   $newArticle.find('h1').text(this.title);
-  $newArticle.find('section.article-body').text(this.body);
+  $newArticle.find('section.article-body').html(this.body);
 
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 /* TODO: This cloned article is no longer a template, as it now
 has real data attached to it. Remove the class from this new article! */
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 
